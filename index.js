fd9ec88fd814cee0b61c5a2e7b4f57ae29ee2e83
mu_zaky <devmuzaky@gmail.com>
2022-08-30 12:35:12 +0200

    add npm commander

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
 const fs = require('fs');
+const { Command } = require('commander');
+const program = new Command();
 
-const [, , action] = process.argv;
-const usersSTR = fs.readFileSync('./users.json', 'utf-8');
-const users = JSON.parse(usersSTR);
+program
+    .name('users-management')
+    .description('CLI to manage users data')
+    .version('0.8.0');
 
-
-const writeUpdatedUsers = (updatedUsers) => {
-    fs.writeFileSync('./users.json', JSON.stringify(updatedUsers, null, 2));
-}
-const addUser = () => {
-    const [, , , name, age] = process.argv;
-    const user = {
-        id: Date.now(),
-        name,
-        age
-    };
-    users.push(user);
-    writeUpdatedUsers(users);
-}
-
-const listUsers = () => {
-    console.log(users);
-}
-
-const deleteUser = () => {
-    const [, , , id] = process.argv;
-    const afterDeleteUser = users.filter(user => user.id !== +id);
-    writeUpdatedUsers(afterDeleteUser);
-}
+// const [, , action] = process.argv;
+// const usersSTR = fs.readFileSync('./users.json', 'utf-8');
+// const users = JSON.parse(usersSTR);
+//
+// const writeUpdatedUsers = (updatedUsers) => {
+//     fs.writeFileSync('./users.json', JSON.stringify(updatedUsers, null, 2));
+// }
+//
+// const addUser = () => {
+//     const [, , , name, age] = process.argv;
+//     const user = {
+//         id: Date.now(),
+//         name,
+//         age
+//     };
+//     users.push(user);
+//     writeUpdatedUsers(users);
+// }
+//
+// const listUsers = () => {
+//     console.log(users);
+// }
+//
 // const deleteUser = () => {
-//     const [ , , , name] = process.argv;
-//     const afterDeleteUser = users.filter(user => user.name !== name);
+//     const [, , , id] = process.argv;
+//     const afterDeleteUser = users.filter(user => user.id !== +id);
 //     writeUpdatedUsers(afterDeleteUser);
 // }
-switch (action) {
-    case 'add':
-        addUser();
-        break;
-    case 'list':
-        listUsers();
-        break;
-    case 'delete':
-        deleteUser();
-        break;
 
-    default:
-        throw new Error('Invalid action');
-}
+// switch (action) {
+//     case 'add':
+//         addUser();
+//         break;
+//     case 'list':
+//         listUsers();
+//         break;
+//     case 'delete':
+//         deleteUser();
+//         break;
+//     default:
+//         throw new Error('Invalid action');
+// }
+
+program.parse();
